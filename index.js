8b07f3319dd8cef97fd97153e6cc55f1f29d00fb
Akash <akashpati89@gmail.com>
2026-05-26 21:22:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-export default function handler(
-  req,
-  res
-) {
-  return res.status(200).json({
-    status: "active",
+{
+  status: "active",
 
-    name:
-      "Rhythmax JioSaavn API",
+name:
+  "JioSaavn API (Vercel)",
 
-    repo:
-      "ODSkyler/js",
+repo:
+  "ODSkyler/js",
 
-    version:
-      "1.0.0",
+version:
+  "1.0.0",
 
-    endpoints: {
-      song_search:
-        "/api/songs?q=",
+endpoints: {
+  song_search:
+      "/api/songs?q=",
 
-      album_search:
-        "/api/albums?q=",
+  album_search:
+      "/api/albums?q=",
 
-      artist_search:
-        "/api/artists?q=",
+  artist_search:
+      "/api/artists?q=",
 
-      playlist_search:
-        "/api/playlists?q=",
+  playlist_search:
+      "/api/playlists?q=",
 
-      artist_details:
-        "/api/artist?token=",
+  artist_details:
+      "/api/artist?token=",
 
-      playlist_details:
-        "/api/playlist?token=",
+  playlist_details:
+      "/api/playlist?token=",
 
-      for_other_endpoints:
-        "check out Saavn API for Cloudflare on my GitHub",
-    },
-  });
+  for_other_endpoints:
+      "check out Saavn API for Cloudflare on my GitHub",
+        }
+  made with ❤️ by: "OD SKYLER",
 }
