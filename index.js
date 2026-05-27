a21e11f09ae997b173a005582d8a533d94f27adf
Akash <akashpati89@gmail.com>
2026-05-27 12:23:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
 {
-  status: "active",
-
-name:
-  "JioSaavn API (Vercel)",
-
-repo:
-  "ODSkyler/js",
-
-version:
-  "1.0.0",
-
+status: "active",
+name: "JioSaavn API",
+repo: "ODSkyler/js",
+version: "1.0.0",
 endpoints: {
-  song_search:
-      "/api/songs?q=",
-
-  album_search:
-      "/api/albums?q=",
-
-  artist_search:
-      "/api/artists?q=",
-
-  playlist_search:
-      "/api/playlists?q=",
-
-  artist_details:
-      "/api/artist?token=",
-
-  playlist_details:
-      "/api/playlist?token=",
-
-  for_other_endpoints:
-      "check out Saavn API for Cloudflare on my GitHub",
-        }
+  song_search: "/api/songs?q=",
+  album_search: "/api/albums?q=",
+  artist_search: "/api/artists?q=",
+  playlist_search: "/api/playlists?q=",
+  album_details: "/api/album?token=",
+  artist_details: "/api/artist?token=",
+  playlist_details: "/api/playlist?token=",
+  related_albums: "/api/related?id="
+   }
   made with ❤️ by: "OD SKYLER",
 }
